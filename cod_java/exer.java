c9490373eadeb8fe9938b9f3a03017f1a22da1a8
Guilherme <guilherme@networkmonteiro.com.br>
2026-03-30 21:12:23 -0300

    Atualização COD-Java

--- cod_java/exer.java
+++ cod_java/exer.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class exer {
-    
+    public static void main(String[] args) {
+        //String nome = "Johnny";
+        //String sobrenome = "Cortez";
+        //System.out.println("Olá " + nome + " " + sobrenome);
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o seu sobrenome");
+        String sobrenome = scanner.nextLine();
+
+        System.out.println("Olá " + nome + " " + sobrenome);
+
+        scanner.close();
+    }
 }
